129f2a3006f7d98a7d61e2810f6f56025e6df798
Juan Simon Abad <juansim87@gmail.com>
2025-04-15 18:27:41 +0200

    html sample

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const ticketContainer = document.getElementById("tickets-container");
 
 const ticketBox = document.createElement("div");
-ticketBox.classList.add("ticket-box");
+// ticketBox.classList.add("ticket");
 
-ticketContainer.appendChild(ticketBox);
+// ticketContainer.appendChild(ticketBox);
 
 const ticketRender = () => {
 	fetch("./utils/tickets.json")
 		.then((res) => res.json())
 		.then((data) => console.log(data))
 		.catch((error) => console.log(error, "Error durante la petición de datos"));
+
+
+        
+        
 };
 
 ticketRender();
